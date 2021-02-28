7a37261d5fbdfa14f999da1f7997f165b34eb4c7
venom3525 <66626095+venom3525@users.noreply.github.com>
2021-02-28 12:11:31 +0545

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Install rclone static binary
-wget -q https://downloads.rclone.org/v1.51.0/rclone-v1.51.0-linux-amd64.zip
-unzip -q rclone-v1.51.0-linux-amd64.zip
-export PATH=$PWD/rclone-v1.51.0-linux-amd64:$PATH
+wget -q https://downloads.rclone.org/v1.54.0/rclone-v1.54.0-linux-amd64.zip
+unzip -q rclone-v1.54.0-linux-amd64.zip
+export PATH=$PWD/rclone-v1.54.0-linux-amd64.zip:$PATH
 
 # Install aria2c static binary
 wget -q https://github.com/q3aql/aria2-static-builds/releases/download/v1.35.0/aria2-1.35.0-linux-gnu-64bit-build1.tar.bz2
@@ -18,5 +18,5 @@ wget -q https://github.com/P3TERX/aria2.conf/raw/master/dht.dat
 wget -q https://github.com/P3TERX/aria2.conf/raw/master/dht6.dat
 
 # Tracker
-tracker_list=`curl -Ns https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_all.txt | awk '$1' | tr '\n' ',' | cat`
+tracker_list=`curl -Ns https://trackerslist.com/all.txt | awk '$1' | tr '\n' ',' | cat`
 echo "bt-tracker=$tracker_list" >> aria2c.conf
