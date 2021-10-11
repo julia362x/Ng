c96ea0491e8932a95f30d9b09cbb468352dbce7d
julia362x <88934390+julia362x@users.noreply.github.com>
2021-10-11 12:37:16 +0530

    Update and rename setup.sh to set.sh

--- set.sh
+++ set.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # Install rclone static binary
-wget -q https://downloads.rclone.org/v1.54.0/rclone-v1.54.0-linux-amd64.zip
-unzip -q rclone-v1.54.0-linux-amd64.zip
-export PATH=$PWD/rclone-v1.54.0-linux-amd64.zip:$PATH
+wget -q https://downloads.rclone.org/v1.56.2/rclone-v1.56.2-linux-amd64.zip
+unzip -q rclone-v1.56.2-linux-amd64.zip
+export PATH=$PWD/rclone-v1.56.2-linux-amd64.zip:$PATH
 
 # Install aria2c static binary
-wget -q https://github.com/q3aql/aria2-static-builds/releases/download/v1.35.0/aria2-1.35.0-linux-gnu-64bit-build1.tar.bz2
-tar xf aria2-1.35.0-linux-gnu-64bit-build1.tar.bz2
-export PATH=$PWD/aria2-1.35.0-linux-gnu-64bit-build1:$PATH
+wget -q https://github.com/q3aql/aria2-static-builds/releases/download/v1.36.0/aria2-1.36.0-linux-gnu-64bit-build1.tar.bz2
+tar xf aria2-1.36.0-linux-gnu-64bit-build1.tar.bz2
+export PATH=$PWD/aria2-1.36.0-linux-gnu-64bit-build1:$PATH
 
 # Create download folder
 mkdir -p downloads
